7fdd08f002ea62b3f3bccf824f133183d52d25c8
Pedro Juan <drom296@gmail.com>
2011-12-07 15:54:08 -0500

    Finished the take a poll

--- day4/take_a_poll.php
+++ day4/take_a_poll.php
@@ -5,13 +5,13 @@
 		'Food Choice' => "What's your fav place to eat in Rochester?" 
 	);
 	
-	if (!array_key_exists('poll', $_GET)){
+	if (!array_key_exists('cat', $_GET)){
 		header("location: choose_a_poll.php");
 	}
 		
 	
 	// decode what was passed in the url to determine the poll.
-	$poll = urldecode($_GET['poll']);
+	$poll = urldecode($_GET['cat']);
 	
 	// set up the choices array
 	$answers = array(
@@ -36,10 +36,37 @@
 		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
 		<link rel="shortcut icon" href="/favicon.ico" />
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+		<style type="text/css">
+		ol{
+			list-style-type:none;
+			margin-left:0;
+			padding-left:0;
+		}
+	</style>
+
 	</head>
 	<body>
 		<h1>Take a Poll</h1>
 		<h2><?php echo $polls[$poll] ?></h2>
+		
+		<form action='poll_results.php' method='GET'>
+			<input type='hidden' name='cat' value=<?php echo $poll ?> />
+			<input type='hidden' name='question' value=<?php echo $polls[$poll] ?>/>
+			<ol>
+			<?php
+				// generate the choices by looping over the array for it
+				
+				foreach ($answers[$poll] as $value) {
+					// setup the list item
+					echo "<li>\n";
+					// setup the input item
+					echo "\t<input type='radio' name = 'choice' value='". $value ."' />$value</li>\n";
+				}
+			?>
+			</ol>
+			<input type="reset" value="Reset Form" />
+			<input type="submit" name="submit" value="Submit Form" />
+		</form>		
 		
 		
 	</body>
