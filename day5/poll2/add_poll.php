bce5c641c4ad81262c277c1627c3aa1d2ae79160
Pedro Mass <drom296@gmail.com>
2011-12-12 23:46:28 -0500

    Increased the level of validation.
    Added a way to check for inputs that are specified in an array.
    
    Also pushed errors to its own array for printing later.

--- day5/poll2/add_poll.php
+++ day5/poll2/add_poll.php
@@ -1,6 +1,83 @@
 <?php
+	// init array that will contain the available fields
+	$fields = array('cat','question','choice1','choice2','choice3','choice4','choice5');
+	// init a errors array
+	$errors = array();
+	// store the method the form is submitted
+	$submitMethod = "POST";
+	// init the passed variable for validation
+	$passed = TRUE; // assumed true until proven false
 	
-
+	// check if the form was submitted
+	// source: http://www.vbforums.com/showthread.php?t=562749
+	if($_SERVER['REQUEST_METHOD'] == $submitMethod) {
+		// initialize a boolean to see if it passed validation	
+		$passed = validateForm();
+	} else{
+		// TODO figure out way to print errors out
+		// echo "Form did not pass validation";
+	}
+	
+	function validateForm(){
+		global $fields;
+		$pass = true;
+		$minLen = 1;
+		
+		// check if the fields are set and are greater than 0 in length
+		foreach ($fields as $field) {
+			// check if the key exists
+			$keyExists = array_key_exists($field, $_POST); 
+			// init values for other comparison variables
+			$isSet = FALSE;
+			$passLen = FALSE;
+			
+			if ($keyExists){
+				// key exists, lets assume other comparison is true until proven false
+				$isSet = false;
+				$keyExists = false;
+				
+				// check if it is set	
+				$isSet = isset($_POST[$field]);
+				// check if it is of min length
+				$passLen = strlen($_POST[$field]) >= $minLen;
+			}
+			// add it to the boolean result via ands
+			$pass = $pass && $keyExists && $isSet && $passLen;
+			
+			echo "<br />$field passed? :";
+			echo " exists? -> ".($keyExists?'True':'False');
+			echo ", isSet? -> ".($isSet?'True':'False');
+			echo ", passLen? -> ".($passLen?'True':'False');
+			
+			// if we failed, init an inner array for $errors
+			if(!$pass){
+				// add errors specfic to the problem
+				if (!$keyExists){
+					$errors[$field] = "input field was not passed";
+				} else if (!$isSet){
+					$errors[$field] = "was not set";
+				} else if (!$passLen){
+					$errors[$field] = "did not meet the minimum length of $minLen";
+				}
+			}
+			
+		}
+		
+		echo "<br /><br /><br />";
+		print_r($errors);
+		
+		// return the result
+		return $pass;
+	}
+	
+	function sanitizeString($var){
+		$var = trim($var);
+		$var = stripslashes($var);
+		$var = htmlentities($var);
+		$var = strip_tags($var);
+		
+		return $var; 
+	}
 ?>
 
 
@@ -12,7 +89,7 @@
 </head>
 <body>
 <h1>Add a Poll</h1>
-<form action = "/~bdfvks/539/day5/add_a_poll.php" method="POST">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 	<table>
 		<tr>
 			<td>Topic Category</td>
@@ -55,8 +132,39 @@
 		
 </form>
 
-<p>
-	</p>
+<p id="errors">
+<?php
+	if (!$passed){
+		// TODO display error message
+		// display error message
+		echo "Did not pass validation for the following reasons";
+		
+		// display errors
+		// start list
+		echo "<ul>";
+	
+		global $errors;	
+	
+		echo "<br />Test2<br />";
+		print_r($GLOBALS['errors']);
+	
+		foreach ($GLOBALS['errors'] as $field => $message) {
+			// start list item
+			echo "<li>";
+			// display error field
+			echo $field;
+			// display separator
+			echo ": ";
+			// display error message
+			echo $message;				
+			// close list item
+			echo "</li>";
+		}
+	}
+	// close list
+	echo "</ul>";
+?>
+</p>
 		<h3><a href="choose_a_poll.php">Choose a Poll</a></h3>
 </body>
 </html>
