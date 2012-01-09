b8e3d9e9c1506c191e1a923b2f058c96e36733c5
Pedro Juan <drom296@gmail.com>
2012-01-09 15:06:01 -0500

    Initial Version of file.

---
+++ day7/volunteer.php
@@ -0,0 +1,26 @@
+<?php
+	define('DATA_URL','students_112.txt');
+	
+	require_once 'volunteer_lib.php';
+	
+	$string = '';
+	$string .= html_header($title='Volunteer Generator'. $styles='');
+	$string .= "<h1>Volunteer Generator</h1>\n";
+	$string .= show_form();
+	$string .= check_submit();
+	$string .= show_people();
+	$string .= html_footer($text='Day 7 HW/ICE');
+	echo string; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="" />
+		<meta name="author" content="Pedro" />
+	</head>
+	<body>
+	
+	</body>
+</html>
