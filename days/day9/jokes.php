4f13c1c7c9a1406f06575f01a29afdd831141daa
unknown <dblab@SL1520-08.(none)>
2012-01-16 14:54:00 -0500

    Finished body, need to work on other.

---
+++ days/day9/jokes.php
@@ -0,0 +1,36 @@
+<?php
+// DOM Methods
+$jokes_url = "http://people.rit.edu/bdfvks/539/day9/jokes_server.php?type=XML&num=5";
+$title = "Great Jokes!";
+
+$dom = new DomDocument();
+$dom -> load($jokes_url);
+
+// get all of the <joke> tags and put them in a DOMNodeList
+$all_jokes = $dom -> getElementsByTagName("joke");
+
+echo "<h1>$title</h1>";
+
+foreach ($all_jokes as $joke) {
+	// get the question
+	$question = $joke -> getElementsByTagName('question') -> item(0) -> nodeValue;
+
+	// get the answer
+	$answer = $joke -> getElementsByTagName('answer') -> item(0) -> nodeValue;
+
+	// get the rating
+	$rating = $joke -> getElementsByTagName('rating') -> item(0) -> nodeValue;
+
+	// display the question
+	echo "\n<h2>$question</h2>";
+
+	// display the answer
+	echo "\n<p><em>$answer</em></p>";
+
+	// display the rating
+	echo "\n<p><small>rating = $rating</small></p>";
+
+	// separate other1
+	echo "\n<hr />\n\n";
+}
+?>
