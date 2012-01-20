ba800a2d6a27d4b3ab8ae4c41f0fd3ddfba1432a
Pedro Mass <drom296@gmail.com>
2012-01-19 19:22:50 -0500

    Initial versions

---
+++ projects/project1/index.php
@@ -0,0 +1,16 @@
+<?php
+    phpinfo();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>html5_template</title>
+		<meta name="description" content="" />
+		<meta name="author" content="Pedro" />
+	</head>
+	<body>
+	
+	</body>
+</html>
