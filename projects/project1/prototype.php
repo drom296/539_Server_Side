f6891799add30a087f4a486f96c8ea7343a7176d
Pedro Mass <drom296@gmail.com>
2012-01-21 17:58:48 -0500

    Improved the CSS

--- projects/project1/prototype.php
+++ projects/project1/prototype.php
@@ -8,21 +8,30 @@
 		<link type="text/css" href="css/nav.css" rel="stylesheet"/>
 		<!-- JavaScript -->
 	</head>
-	<body> 
+	<body>
 		<div id="page">
-			<div id="banner">
-				<img src="img/logo.png" alt="logo" />
+			<div id="header">
+				<div id="banner">
+					<img src="img/logo.png" alt="logo" />
+				</div>
+				<div id="navDiv">
+					<ul id="nav">
+						<li id="navHome">
+							<a href="">Home</a>
+						</li>
+						<li id="navNews">
+							<a href="">News</a>
+						</li>
+						<li id="navAdmin">
+							<a href="">Admin</a>
+						</li>
+						<li id="navAbout">
+							<a href="">About</a>
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div id="nav">
-				<ul id="nav">
-					<li id="navHome"><a href="">Home</a></li>
-					<li id="navNews"><a href="">News</a></li>
-					<li id="navAdmin"><a href="">Admin</a></li>
-					<li id="navAbout"><a href="">About</a></li>
-				</ul>
-			</div>
-			<div id="content" class="noFloat">
-				
+			<div id="content" class="noFloat roundBox">
 				<div id="editorial" class="navHome">
 					<h1>Editorial</h1>
 					<img src="img/editor2.jpg" alt="editor" />
@@ -41,21 +50,20 @@
 						<br/>
 					</p>
 				</div>
-				
-				<div id="news">
+				<div id="news" class="roundBox">
 					<h1>News</h1>
 					<?php
-					$times = 10;
+					$times = 2;
 					for ($i = 0; $i < $times; $i++) {
 						$string = <<<END
 <div class="newsItem">
-						<p>
-							<span class="newsSubject">Subject</span><span class="newsDate">1/12/2012</span>
-						</p>
-						<p>
-							This is a story, of a lovely lady. Who had more kids than a grown man can count.
-						</p>
-					</div>\n\t\t\t\t\t
+<p>
+<span class="newsSubject">Subject</span><span class="newsDate">1/12/2012</span>
+</p>
+<p>
+This is a story, of a lovely lady. Who had more kids than a grown man can count.
+</p>
+</div>\n\t\t\t\t\t
 END;
 						echo $string;
 					}
