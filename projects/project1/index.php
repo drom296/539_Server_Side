b72edff377c6a13d95e08f292fa9cebefa7cd75a
Pedro Mass <drom296@gmail.com>
2012-01-21 20:50:02 -0500

    updated titles

--- projects/project1/index.php
+++ projects/project1/index.php
@@ -4,7 +4,7 @@ require("LIB_project1.php");
 $styles = array("css/pedro.css", "css/nav.css");
 
 // create header tags
-$output = html_header("Index", $styles);
+$output = html_header("Pedro News - Home", $styles);
 
 // create banner div
 $output .= addBanner();
