ec23591fedab1900923755022efee565789245e1
Pedro Juan <drom296@gmail.com>
2012-01-30 19:43:18 -0500

    Initial files for day13

--- projects/project1/prototype.php
+++ projects/project1/prototype.php
@@ -8,21 +8,28 @@
 		<link type="text/css" href="css/nav.css" rel="stylesheet"/>
 		<!-- JavaScript -->
 	</head>
-	<body> 
+	<body>
 		<div id="page">
 			<div id="banner">
 				<img src="img/logo.png" alt="logo" />
 			</div>
 			<div id="nav">
 				<ul id="nav">
-					<li id="navHome"><a href="">Home</a></li>
-					<li id="navNews"><a href="">News</a></li>
-					<li id="navAdmin"><a href="">Admin</a></li>
-					<li id="navAbout"><a href="">About</a></li>
+					<li id="navHome">
+						<a href="">Home</a>
+					</li>
+					<li id="navNews">
+						<a href="">News</a>
+					</li>
+					<li id="navAdmin">
+						<a href="">Admin</a>
+					</li>
+					<li id="navAbout">
+						<a href="">About</a>
+					</li>
 				</ul>
 			</div>
 			<div id="content" class="noFloat">
-				
 				<div id="editorial" class="navHome">
 					<h1>Editorial</h1>
 					<img src="img/editor2.jpg" alt="editor" />
@@ -41,7 +48,6 @@
 						<br/>
 					</p>
 				</div>
-				
 				<div id="news">
 					<h1>News</h1>
 					<?php
@@ -49,13 +55,13 @@
 					for ($i = 0; $i < $times; $i++) {
 						$string = <<<END
 <div class="newsItem">
-						<p>
-							<span class="newsSubject">Subject</span><span class="newsDate">1/12/2012</span>
-						</p>
-						<p>
-							This is a story, of a lovely lady. Who had more kids than a grown man can count.
-						</p>
-					</div>\n\t\t\t\t\t
+<p>
+<span class="newsSubject">Subject</span><span class="newsDate">1/12/2012</span>
+</p>
+<p>
+This is a story, of a lovely lady. Who had more kids than a grown man can count.
+</p>
+</div>\n\t\t\t\t\t
 END;
 						echo $string;
 					}
