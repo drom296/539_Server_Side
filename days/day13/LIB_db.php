9e1048ed7f92de7e482f372924d94b791233620d
Pedro Mass <drom296@gmail.com>
2012-01-30 21:14:20 -0500

    added function to handle adding links for people info

--- days/day13/LIB_db.php
+++ days/day13/LIB_db.php
@@ -5,6 +5,11 @@
 // Send a Query to the Selected Database
 // Retrieve The Results of the Query
 // Close the Connection to the Database
+define("ID_FIELD","PersonID");
+define("FIRST_NAME_FIELD","FirstName");
+define("LAST_NAME_FIELD","LastName");
+define("NICK_NAME_FIELD","NickName");
+
 
 function getPeopleInfo($host, $user, $password, $database, $query) {
 	$records = array();
@@ -46,7 +51,7 @@ function getPeopleInfo($host, $user, $password, $database, $query) {
 	return $records;
 }
 
-function printInfo($host, $user, $password, $database, $query) {
+function printInfo($host, $user, $password, $database, $query, $forPeople = false) {
 	// make sure we have parameters passed
 	if (empty($host) || empty($user) || empty($password) || empty($database) || empty($query)) {
 		return false;
@@ -55,35 +60,71 @@ function printInfo($host, $user, $password, $database, $query) {
 	// Retrieve The Results of the Query
 	$records = getPeopleInfo($host, $user, $password, $database, $query);
 
-	// start the table
-	$result = "<table border='1'>";
+	$result = "<h1>" . count($records) . " records found!</h1>\n";
 
 	// get the header info by geting the fields
 	if (is_array($records) && !empty($records)) {
+		// start the table
+		$result .= "<table border='1'>\n";
+
+		//grab the fields
 		$fields = array_keys($records[0]);
 		$result .= "\t<tr>\n";
+		// display the fields
 		foreach ($fields as $field) {
 			$result .= "\t\t<th valign='top'>$field</th>\n";
 		}
 		$result .= "\t</tr>\n";
-	}
 
-	// loop through the people
-	foreach ($records as $record) {
-		$result .= "\t<tr>\n";
+		// loop through the people
+		foreach ($records as $record) {
+			$result .= "\t<tr>\n";
 
-		foreach ($record as $field => $value) {
-			$result .= "\t\t<td valign='top'>$value</td>\n";
+			// check if they want a link for people
+			if ($forPeople) {
+				$result .= getLink($record);
+			} else {
+				// just loop thru each of the fields
+				foreach ($record as $field => $value) {
+					$result .= "\t\t<td valign='top'>$value</td>\n";
+				}
+			}
+
+			// commence the row
+			$result .= "\t</tr>\n";
 		}
 
-		// commence the row
-		$result .= "\t</tr>\n";
+		// end the table
+		$result .= "</table>\n";
 	}
-
-	// end the table
-	$result .= "</table>";
 
 	// echo result
 	echo $result;
+}
+
+function getLink($record) {
+	$result = "";
+	
+	// get the id
+	$id = $record['PersonID'];
+	
+	// get the first name
+	$lName = $record['LastName'];
+	
+	// get the last name
+	$fName = $record['FirstName'];
+	
+	// get the nickname
+	$nName = $record['NickName'];
+	
+	// start data
+	$result .= "\t\t<td><a href='phones.php?".ID_FIELD."=$id&"
+							.FIRST_NAME_FIELD."=$fName&".LAST_NAME_FIELD."=$lName>$id</a></td>";
+	$result .= "\t\t<td>$lName</td>";
+	$result .= "\t\t<td>$fName</td>";
+	$result .= "\t\t<td>$nName</td>";
+	
+	// return the result	
+	return $result;
 }
 ?>
