f0c75761529236f28417f618f3d0353518c4c1cc
Pedro Mass <drom296@gmail.com>
2012-01-30 21:14:03 -0500

    displays all phone numbers

---
+++ days/day13/phones.php
@@ -0,0 +1,13 @@
+<?php
+require_once ('LIB_db.php');
+
+// TASKS:
+// grab the people from the people table
+// display in a table
+$host = 'localhost';
+$user = 'pjm8632';
+$password = 'Bully296';
+$database = "pjm8632";
+$query = "select * from phonenumbers";
+printInfo($host, $user, $password, $database, $query, true);
+?>
