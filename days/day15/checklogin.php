ee8f34c70d3bd749123ef672428403a6154db79c
Pedro Juan <drom296@gmail.com>
2012-02-06 15:54:04 -0500

    Added logout feature

--- days/day15/checklogin.php
+++ days/day15/checklogin.php
@@ -1,10 +1,21 @@
 <?php
-$username="admin";
-$password="admin";
-if($_POST['username'] == $username && $_POST['password'] == $password){
+$username = "admin";
+$password = "admin";
+
+$expire = time() + 60 * 2;
+// two minutes from now
+$path = "/~pjm8643/";
+$domain = "nova.it.rit.edu";
+$secure = false;
+
+if ($_POST['username'] == $username && $_POST['password'] == $password) {
+	// set the cookies
+	setcookie("username", $_POST['username'], $expire, $path, $domain, $secure);
+	
+	setcookie("password", $_POST['password'], $expire, $path, $domain, $secure);
+
 	header("Location:success.php");
 } else {
 	header("Location:http://www.wizards.com");
 }
-
 ?>
