7436558d484fad54238f230eb850671b7c214a45
Pedro Mass <drom296@gmail.com>
2012-02-29 17:08:07 -0500

    Added some formatting comments

--- projects/project3b/submit_ad.php
+++ projects/project3b/submit_ad.php
@@ -1,90 +1,101 @@
 <?php
-session_start();
-ob_start();
+    session_start();
+    ob_start();
 
-require ("LIB_project1.php");
+    require ("LIB_project1.php");
 
-$captchaLoc = "js/qaptcha/";
+    $captchaLoc = "js/qaptcha/";
 
-$styles = array("css/pedro.css", "css/nav.css", "css/form.css", $captchaLoc."jquery/QapTcha.jquery.css");
+    // @formatter:off
+    $styles = array("css/pedro.css", "css/nav.css", 
+                    "css/form.css", $captchaLoc . "jquery/QapTcha.jquery.css");
+    // @formatter:on
 
-$scripts = array($captchaLoc."jquery/jquery.js", $captchaLoc."jquery/jquery-ui.js", 
-$captchaLoc."jquery/jquery.ui.touch.js", $captchaLoc."jquery/QapTcha.jquery.js", 'js/pedro_captcha.js');
+    // @formatter:off
+    $scripts = array($captchaLoc . "jquery/jquery.js", 
+                        $captchaLoc . "jquery/jquery-ui.js", 
+                        $captchaLoc . "jquery/jquery.ui.touch.js", 
+                        $captchaLoc . "jquery/QapTcha.jquery.js", 
+                        'js/pedro_captcha.js');
+    // @formatter:on
 
-// var_dump($scripts);
+    // var_dump($scripts);
 
+    // create header tags
+    $output = html_header("Submit Your Ad Here", $styles, $scripts);
 
-// create header tags
-$output = html_header("Submit Your Ad Here", $styles, $scripts);
+    // create banner div
+    $output .= addBanner();
 
-// create banner div
-$output .= addBanner();
+    // create the nav
+    $output .= addNav();
 
-// create the nav
-$output .= addNav();
+    ////// CONTENT
+    $output .= startContentDiv();
 
-////// CONTENT
-$output .= startContentDiv();
+    // required fields
+    $reqFields = array("editions", "content", 'title');
 
-// required fields
-$reqFields = array("editions", "content", 'title');
+    // check if they submitted the form
+    if (isset($_GET['submit'])) {
 
-// check if they submitted the form
-if (isset($_GET['submit'])) {
+        // this works on my localhost but not on gibson
+        // i guess i'll comment it out for gibson
 
-	// this works on my localhost but not on gibson
-	// i guess i'll comment it out for gibson
+        // check if they passed the captcha
+        // @formatter:off
+        if (isset($_SESSION['qaptcha_key']) && 
+            !empty($_SESSION['qaptcha_key'])) {
+        // @formatter:on
 
-	// check if they passed the captcha
-	if (isset($_SESSION['qaptcha_key']) && !empty($_SESSION['qaptcha_key'])) {
-		
-		$myVar = $_SESSION['qaptcha_key'];
-		if (isset($_POST['' . $myVar . '']) && empty($_POST['' . $myVar . ''])) {
-			// they did not passed the captcha
-			$output .= '<div class="submitAdResponse submitError">Failure: You must be a human to use this form.</div>';
-		} else {
-			// they passed the captcha
+            $myVar = $_SESSION['qaptcha_key'];
+            if (isset($_POST['' . $myVar . '']) && empty($_POST['' . $myVar . ''])) {
+                // they did not passed the captcha
+                $output .= '<div class="submitAdResponse submitError">Failure: 
+                                You must be a human to use this form.</div>';
+            } else {
+                // they passed the captcha
 
-			// check if they submitted everything
-			if (arrayContainsVals($_GET, $reqFields)) {
+                // check if they submitted everything
+                if (arrayContainsVals($_GET, $reqFields)) {
 
-				// implode the editions array
-				$_GET['editions'] = implode(",", $_GET['editions']);
+                    // implode the editions array
+                    $_GET['editions'] = implode(",", $_GET['editions']);
 
-				// start building the post array
-				$post = array();
-				foreach ($reqFields as $field) {
-					$post[$field] = $_GET[$field];
-				}
+                    // start building the post array
+                    $post = array();
+                    foreach ($reqFields as $field) {
+                        $post[$field] = $_GET[$field];
+                    }
 
-				// make the request using MyCurl class
-				$response = submitAd($post);
+                    // make the request using MyCurl class
+                    $response = submitAd($post);
 
-				// display the result
-				$output .= '<div class="submitAdResponse">' . $response['output'] . '</div>';
+                    // display the result
+                    $output .= '<div class="submitAdResponse">' . $response['output'] . '</div>';
 
-			} else {
-				// they did not pass
+                } else {
+                    // they did not pass
 
-				$output .= '<div class="submitAdResponse submitError">Failed to submit ad. All fields are required.</div>';
-			}
-		}
-	} else{
-		// they did not passed the captcha
-			$output .= '<div class="submitAdResponse submitError">Failure: You must be a human to use this form.</div>';
-	}
-	unset($_SESSION['qaptcha_key']);
-}
+                    $output .= '<div class="submitAdResponse submitError">Failed to submit ad. All fields are required.</div>';
+                }
+            }
+        } else {
+            // they did not passed the captcha
+            $output .= '<div class="submitAdResponse submitError">Failure: You must be a human to use this form.</div>';
+        }
+        unset($_SESSION['qaptcha_key']);
+    }
 
-$output .= displaySubmitAdForm();
+    $output .= displaySubmitAdForm();
 
-$output .= closeContentDiv();
-///// FOOTER
+    $output .= closeContentDiv();
+    ///// FOOTER
 
-// create footer
-$output .= html_footer("");
+    // create footer
+    $output .= html_footer("");
 
-echo $output;
+    echo $output;
 
-ob_flush();
+    ob_flush();
 ?>
